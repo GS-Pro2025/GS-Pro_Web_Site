5d2850254c5d7c31b02f3b5265b5a9547e176af8
Juliandross <138513671+Juliandross2@users.noreply.github.com>
2025-03-04 00:27:40 -0500

    navbar_v2_responsive

--- GS_Pro_WebSite/src/components/Navbar.jsx
+++ GS_Pro_WebSite/src/components/Navbar.jsx
@@ -1,44 +1,42 @@
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import "./navbar.css";
+import { useState } from 'react';
+import './navbar.css';
 import logo from "../assets/logo.png";
 import buttonContactUs1 from "../assets/buttonContactUs1.png";
 import buttonContactUs2 from "../assets/buttonContactUs2.png";
 
 const Navbar = () => {
-    const [arrow, setArrow] = useState(buttonContactUs1); // Estado para la imagen
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [contactImage, setContactImage] = useState(buttonContactUs1);
 
     return (
         <nav className="navbar">
             <div className="container">
                 <div className="logo-container">
-                    <img src={logo} id="logo" alt="Logo" />
+                    <img src={logo} id="logo" alt="Logo"/>
                 </div>
-                <div className="nav-elements">
-                    <ul>
-                        <li>
-                            <button className="HeaderOptions" id="mision">Misión</button>
-                        </li>
-                        <li>
-                            <button className="HeaderOptions" id="vision">Visión</button>
-                        </li>
-                        <li>
-                            <button className="HeaderOptions" id="servicios">Servicios</button>
-                        </li>
-                        <li>
-                            <button 
-                                className="HeaderOptions contact-button"
-                                onMouseEnter={() => setArrow(buttonContactUs2)}
-                                onMouseLeave={() => setArrow(buttonContactUs1)}
-                            >
-                                <img src={arrow} alt="Contáctenos" className="arrow-icon" />
-                            </button>
-                        </li>
-                    </ul>
+
+                {/* Botón de menú para móviles */}
+                <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+                    ☰
+                </button>
+
+                {/* Menú de opciones */}
+                <div className={`nav-elements ${menuOpen ? 'active' : ''}`}>
+                    <button className="HeaderOptions">Misión</button>
+                    <button className="HeaderOptions">Visión</button>
+                    <button className="HeaderOptions">Servicios</button>
+                    
+                    {/* Botón de Contacto con efecto hover */}
+                    <button className="contact-button"
+                        onMouseEnter={() => setContactImage(buttonContactUs2)}
+                        onMouseLeave={() => setContactImage(buttonContactUs1)}
+                    >
+                        <img src={contactImage} alt="Contáctenos" />
+                    </button>
                 </div>
             </div>
         </nav>
     );
-}
+};
 
 export default Navbar;
