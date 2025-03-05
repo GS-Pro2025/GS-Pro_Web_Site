5bf78e81fd3b92e31ced078ce997eb5a356cf50d
DanielMedina003 <162385548+DanielMedina003@users.noreply.github.com>
2025-03-04 23:32:08 -0500

    Componente home ready

--- GS_Pro_WebSite/src/components/Home.jsx
+++ GS_Pro_WebSite/src/components/Home.jsx
@@ -1,55 +1,56 @@
 const Home = () => {
   return (
-    <div
-      className="relative h-screen bg-cover bg-center bg-[url('src/assets/azul-537.png')]"
-    >
-      <div className="absolute inset-0 flex">
-        {/* Div Izquierdo - Mensaje de Bienvenida y Cuadro de Texto */}
-        <div className="w-1/2 p-10 flex flex-col justify-center">
-          <div className="text-white mb-6">
-            <h1 className="text-4xl font-bold">GS PRO - MASTER <p></p>
-            The perfect ally for your move</h1>
-            <h2 className="text-lg mt-2">
-              Solicuta su Cotización gratis.
-            </h2>
-            <h3>1-475-343-asdasdasdasdasdasdasdasdads9497</h3>
-            <p>
-
-            </p>
-          </div>
+    <div className="relative min-h-screen bg-cover bg-center bg-[url('src/assets/azul-537.png')] grid grid-cols-1 md:grid-cols-2 font-roboto">
+      {/* Mensaje de Bienvenida - Aparece primero en móviles */}
+      <div className="p-6 md:p-10 flex flex-col justify-center text-white text-center md:text-left md:order-last relative md:top-[-125px] md:left-[-150px]">
+        <div className="flex justify-center md:justify-start">
+          <img 
+            src="src/assets/GS-PRO-Blanco.webp" 
+            alt="GS PRO Logo" 
+            className="w-65 md:w-65 relative md:right-[-215px]"
+          />
         </div>
+        <h2 className="text-lg md:text-xl mt-2 text-center">
+          Welcome to GS-PRO MASTER <br />
+          We take care of your move, you enjoy the change.
+        </h2>
+      </div>
 
-        {/* Div Derecho - Formulario de Cotización */}
-        <div className="w-1/2 p-10 flex items-center justify-center ">
-          <div className="bg-white p-6 rounded-lg shadow-xl max-w-md w-full border-2 border-red-500">
-            <h2 className="text-xl font-bold">Cotización de mudanzas</h2>
-            <p className="text-gray-600">We ship within 2 working days</p>
-            <input
-              type="text"
-              placeholder="Desde: Código postal"
-              className="w-full border p-2 mt-2 rounded-md"
-            />
-            <input
-              type="text"
-              placeholder="Hasta: Código postal"
-              className="w-full border p-2 mt-2 rounded-md"
-            />
-            <button className="bg-blue-600 text-white w-full mt-4 py-2 rounded-lg hover:bg-blue-800">
-              Cotiza aquí
-            </button>
-          </div>
+      {/* Formulario de Cotización */}
+      <div className="p-6 md:p-10 flex items-center justify-center relative md:top-[-125px] md:left-[-150px]">
+        <div className="bg-white p-6 rounded-lg shadow-xl w-[350px] min-h-[350px] border-2 border-[#2E1EA5]">
+          <h2 className="text-xl md:text-2xl font-bold">Moving Quote</h2>
+          <p className="text-gray-600">We ship within 2 working days</p>
+
+          <label className="block text-sm font-semibold mt-3 flex">Since:</label>
+          <input
+            type="text"
+            placeholder="Zip code"
+            className="w-full border p-2 mt-2 rounded-md"
+          />
+
+          <label className="block text-sm font-semibold mt-3 flex">Until:</label>
+          <input
+            type="text"
+            placeholder="Zip code"
+            className="w-full border p-2 mt-2 rounded-md"
+          />
+          
+          <button className="bg-[#2E1EA5] text-white w-full mt-4 py-2 rounded-lg hover:bg-blue-800">
+          Get a quote here 
+          </button>
         </div>
       </div>
 
       {/* Botón flotante de WhatsApp */}
       <a
         href="https://wa.me/123456789"
-        className="fixed bottom-5 right-5 bg-green-500 p-3 rounded-full shadow-lg"
+        className="fixed bottom-3 right-3 rounded-full shadow-lg"
       >
-        <img src="/assets/whatsapp.png" alt="WhatsApp" className="w-10" />
+        <img src="src/assets/whatsapp.webp" alt="WhatsApp" className="w-9 h-9" />
       </a>
     </div>
   );
 };
 
-export default Home;
+export default Home;
