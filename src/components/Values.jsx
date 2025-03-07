180f4421442147093ef47cb1251a56acc876e8d0
Juliandross <138513671+Juliandross2@users.noreply.github.com>
2025-03-06 22:36:45 -0500

    first-commit-values

---
+++ src/components/Values.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import securityGear from "../assets/securityGear.svg"; 
+import securityGearText from "../assets/securityGearText.svg"; 
+import punctualityGear from "../assets/punctualityGear.svg"; 
+import punctualityGearText from "../assets/punctualityGearText.svg"; 
+import compromiseGear from "../assets/compromiseGear.svg"; 
+import compromiseGearText from "../assets/compromiseGearText.svg"; 
+import responsabilityGear from "../assets/responsabilityGear.svg"; 
+import responsabilityGearText from "../assets/responsabilityGearText.svg"; 
+const Values = () => {
+    <>
+        <img src={securityGear} alt="" />
+        <img src={punctualityGear} alt="" />
+        <img src={compromiseGear} alt="" />
+        <img src={responsabilityGear} alt="" />
+    </>
+}
+
+export default Values; 
