7773e62e252d2521aacc0feab6931eb11b532df1
Manager-Support@gspromastermoving.com <Manager-Support@gspromastermoving.com>
2025-03-11 18:32:03 -0500

    exchange images service

--- src/components/About_us.jsx
+++ src/components/About_us.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/lg-negro.png";
 
 export default function AboutUs() {
   return (
-    <div className="flex flex-col items-center px-4">
+    <div className="flex flex-col items-center px-4 mb-4 mt-9">
       {/* Sección Principal */}
       <div className="max-w-5xl flex flex-col md:flex-row items-center gap-6 p-4">
         <img
@@ -14,59 +14,66 @@ export default function AboutUs() {
         <div className="md:w-2/3 text-center md:text-left">
           <h2 className="text-3xl font-bold text-[#0458AB]">🌟 SOBRE NOSOTROS</h2>
           <p className="mt-2 text-gray-600 text-sm leading-relaxed">
-            Nuestra empresa nace desde el 2018 para ofrecer un servicio de mudanzas principalmente de casas, 
-            entendiendo que cada hogar tiene historias, recuerdos y pertenencias que merecen el máximo cuidado. 
+            Nuestra empresa nace desde el 2018 para ofrecer un servicio de mudanzas principalmente de casas,
+            entendiendo que cada hogar tiene historias, recuerdos y pertenencias que merecen el máximo cuidado.
             GS PRO MASTER MOVING tiene como objetivo hacer que el proceso de mudanza sea lo más fácil y libre de estrés.
           </p>
           <div className="bg-gray-100 text-center p-3 mt-3 rounded-lg shadow-lg border-l-4 border-blue-500">
             <p className="text-gray-700 text-sm font-medium">
-              Sabemos lo que significa mudarse con niños, mascotas y toda una vida de recuerdos, 
-              por eso nos comprometemos a brindarte un servicio personalizado y confiable. 
-              Creemos que el cambio de hogar debe ser una experiencia positiva para toda la familia 
-              y nos complace como empresa ser parte de este proceso. Contamos con personal capacitado 
+              Sabemos lo que significa mudarse con niños, mascotas y toda una vida de recuerdos,
+              por eso nos comprometemos a brindarte un servicio personalizado y confiable.
+              Creemos que el cambio de hogar debe ser una experiencia positiva para toda la familia
+              y nos complace como empresa ser parte de este proceso. Contamos con personal capacitado
               y vehículos adecuados, adaptándonos siempre a las necesidades de cada familia.
             </p>
           </div>
         </div>
       </div>
 
-{/* Contenedor formulario */}
-<div className="w-screen bg-[#0458AB] py-13 md:h-64.5">
-  <div className="max-w-lg mx-auto px-4">
-  <motion.div
-    className="text-center text-white"
-    initial={{ opacity: 0, y: 20 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5 }}
-  >
-    <h2 className="text-xl md:text-2xl font-semibold mb-4">Moving Quote on Demand:</h2>
+      {/* Contenedor formulario */}
+      <div className="w-full bg-[#0458AB] py-13 md:h-64.5">
+        <h3 className="text-lg  text-white md:text-xl font-semibold mb-4">Fast, safe and professional -
+          that's moving with GS Pro Master Moving!
+          Get a quote today and leave your move in expert hands.”</h3>
+        <div className="max-w-lg mx-auto px-4">
+          <motion.div
+            className="text-center text-white"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
 
-    <div className="grid md:grid-cols-2 gap-3">
-      <div className="flex flex-col items-start">
-        <label className="text-xs md:text-sm mb-1 font-light">From:</label>
-        <input
-          type="text"
-          placeholder="Zip code"
-          className="w-full p-2 rounded-md bg-[#003B73] text-white placeholder-blue-200 text-xs border border-[#60A3D9] focus:outline-none focus:ring-2 focus:ring-blue-300"
-        />
+            <h2 className="text-lg md:text-xl font-semibold mb-4">Moving Quote on Demand:</h2>
+
+            <div className="grid md:grid-cols-2 gap-3">
+              <div className="flex  items-start">
+                <label className="text-base md:text-sm mr-4 mb-1 font-light">From: </label>
+                <input
+                  type="text"
+                  placeholder="Zip code"
+                  className="w-full p-2 rounded-md bg-[#003B73] text-white placeholder-blue-200 text-xs border border-[#60A3D9] focus:outline-none focus:ring-2 focus:ring-blue-300"
+                />
+              </div>
+
+              <div className="flex items-start">
+                <label className="text-base md:text-sm mr-4 mb-1 font-light">To: </label>
+                <input
+                  type="text"
+                  placeholder="Zip code"
+                  className="w-full p-2 rounded-md bg-[#003B73] text-white placeholder-blue-200 text-xs border border-[#60A3D9] focus:outline-none focus:ring-2 focus:ring-blue-300"
+                />
+              </div>
+            </div>
+            <button class="cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg mt-4 
+            border-blue-600
+              border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
+              active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
+              Get Your Free Quote
+            </button>
+
+          </motion.div>
+        </div>
       </div>
-
-      <div className="flex flex-col items-start">
-        <label className="text-xs md:text-sm mb-1 font-light">To:</label>
-        <input
-          type="text"
-          placeholder="Zip code"
-          className="w-full p-2 rounded-md bg-[#003B73] text-white placeholder-blue-200 text-xs border border-[#60A3D9] focus:outline-none focus:ring-2 focus:ring-blue-300"
-        />
-      </div>
-    </div>
-
-    <button className="bg-[#003B73] hover:bg-[#60A3D9] text-white font-bold py-1.5 px-5 rounded-lg mt-4 text-xs transition-colors duration-300">
-      Get Your Free Quote
-    </button>
-  </motion.div>
-</div>
-</div>
 
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,63 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";  
-import logo from "../assets/logo.webp";
-import buttonContactUs1 from "../assets/buttonContactUs1.webp";
-import buttonContactUs2 from "../assets/buttonContactUs2.webp";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import logoazul from "../assets/logoazul.webp";
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
-    const [contactImage, setContactImage] = useState(buttonContactUs1);
-    const [navbarBg,] = useState("bg-transparent shadow-md");
-    const [currentLogo, setCurrentLogo] = useState(logo);
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 50) {
-                setCurrentLogo(logoazul);
-            } else {
-                setCurrentLogo(logo);
-            }
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
 
     return (
-        <nav className={`fixed z-50 w-full flex items-center p-3 transition-all duration-300 ${navbarBg} top-0 left-0`}>
+        <nav className="fixed z-50 w-full flex items-center p-2 transition-all duration-300 bg-white/80 shadow-md top-0 left-0">
             <div className="max-w-[1200px] mx-auto px-6 flex items-center justify-between w-full h-full">
                 {/* Logo */}
                 <Link to="/">
-                    <img src={currentLogo} alt="Logo" className="h-16 w-auto md:h-24" />
+                    <img src={logoazul} alt="Logo" className="h-12 w-auto md:h-16" />
                 </Link>
 
-                {/* Movile burguer menu*/}
-                <button 
-                    className="text-[#0458AB] text-3xl md:hidden" 
+                {/* Mobile burger menu */}
+                <button
+                    className="text-[#0458AB] text-3xl md:hidden"
                     onClick={() => setMenuOpen(!menuOpen)}
                     aria-label="Toggle menu"
                 >
                     {menuOpen ? "✖" : "☰"}
                 </button>
 
-                {/* Nav - menu*/}
-                <div className={`absolute top-20 left-0 w-full bg-white md:static md:bg-transparent md:flex md:items-center md:space-x-10 md:justify-end ${menuOpen ? "block" : "hidden"}`}>
+                {/* Nav - menu */}
+                <div
+                    className={`absolute top-16 left-0 w-full bg-white md:static md:bg-transparent md:flex md:items-center md:space-x-10 md:justify-end ${menuOpen ? "block" : "hidden"
+                        }`}
+                >
                     <div className="flex flex-col md:flex-row md:items-center w-full md:w-auto text-[#0458AB]">
-                        <Link to="/aboutUs" className="text-[#0458AB] text-xl font-semibold cursor-pointer px-6 py-3 block md:inline">About us</Link>
-                        <Link to="/team" className="text-[#0458AB] text-xl font-semibold cursor-pointer px-6 py-3 block md:inline">Team</Link>
-                        <Link to="/vacancies" className="text-[#0458AB] text-xl font-semibold cursor-pointer px-6 py-3 block md:inline">Vacancies</Link>
+                        <Link
+                            to="/aboutUs"
+                            className="text-[#0458AB] text-xl font-semibold cursor-pointer px-6 py-3 block md:inline"
+                        >
+                            About us
+                        </Link>
+                        <Link
+                            to="/team"
+                            className="text-[#0458AB] text-xl font-semibold cursor-pointer px-6 py-3 block md:inline"
+                        >
+                            Team
+                            <span className="absolute left-0 bottom-0 w-full h-[3px] bg-[#0458AB] scale-x-0 group-hover:scale-x-100 group-hover:bg-sky-500 transition-transform duration-300"></span>
+                        </Link>
+                        <Link
+                            to="/vacancies"
+                            className="text-[#0458AB] text-xl font-semibold cursor-pointer px-6 py-3 block md:inline"
+                        >
+                            Vacancies
+                        </Link>
+                        <Link
+                            to="/contactUs"
+                            className="bg-[#003B73] text-sky-400 border border-sky-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group"
+                        >
+                            <span className="bg-sky-400 shadow-sky-400 absolute -top-[150%] left-0 inline-flex w-80 h-[2px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+                            Contact us
+                        </Link>
                     </div>
                 </div>
 
                 {/* Contact button */}
-                <Link
-                to="/contactUs"
-                className="relative flex items-center px-10 py-1 overflow-hidden font-medium transition-all bg-indigo-500 rounded-md group"
-                >
-                <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-mr-4 group-hover:-mt-4">
-                    <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
-                </span>
-                <span className="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-ml-4 group-hover:-mb-4">
-                    <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
-                </span>
-                <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-indigo-600 rounded-md group-hover:translate-x-0"></span>
-                <span className="relative w-full text-center text-white transition-colors duration-200 ease-in-out group-hover:text-white">
-                    Contact us
-                </span>
-                </Link>
             </div>
         </nav>
     );
