c88efde2dae3f860dbcb36f8d021ad8cc847b381
Juliandross <138513671+Juliandross2@users.noreply.github.com>
2025-03-12 11:13:04 -0500

    quoteFlow_v1

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/HomePage';
 import AboutUsPage from './pages/AboutUsPage';
 import ContactUsPage from './pages/contactUsPage';
 import VacanciesPage from './pages/VacanciesPage';
+
 const App = () => {
   return (
     
