13b59ab79d7bd38dc8f5587884622281fd4dd980
DanielMedina003 <162385548+DanielMedina003@users.noreply.github.com>
2025-03-13 16:46:30 -0500

    Revert "correcion de idioma"
    This reverts commit 46a739ca02020c382e5b6881cde2518cc7d19026.

--- src/components/QuoteForm.jsx
+++ src/components/QuoteForm.jsx
@@ -1,7 +1,7 @@
 const QuoteForm = ({ setShowForm }) => {
   return (
     <div className="font-sans bg-[#0458AB]/90 p-3 text-white w-full max-w-xs md:w-80 md:h-80 border-2 border-[#1a237e] rounded-4xl translate-y-60 md:translate-x-20 md:translate-y-[-10px] mx-auto md:mx-0">
-      <h3 className="text-lg md:text-xl mb-2 text-center md:text-center">Moving quote</h3>
+      <h3 className="text-lg md:text-xl mb-2 text-center md:text-center">Cotización de mudanzas</h3>
       <p className="text-sm mb-4 text-center md:text-left">We ship within 2 working days</p>
 
       <div className="mb-4">
