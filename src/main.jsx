c93e714dcf6ef136a51600c91d310e301d4a4446
Manager-Support@gspromastermoving.com <Manager-Support@gspromastermoving.com>
2025-03-17 11:36:21 -0500

    fix basename in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/GS-Pro_Web_Site">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/GS-Pro_Web_Site/', 
+  base: './', 
   build: {
     outDir: 'dist', 
   }
